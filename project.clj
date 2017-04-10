041eb54e572465e7315f6c88af7c943569cab449
Ian McEwen <mian@cyverse.org>
2017-04-10 15:36:20 -0700

    Upgrade version and clj-jargon dependency

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
       (string/trim (:out (sh "git" "rev-parse" "HEAD")))
       ""))
 
-(defproject org.cyverse/porklock "2.8.1-SNAPSHOT"
+(defproject org.cyverse/porklock "2.12.0-SNAPSHOT"
   :description "A command-line tool for interacting with iRODS."
   :url "https://github.com/cyverse-de/porklock"
   :license {:name "BSD"
@@ -22,6 +22,6 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [commons-io/commons-io "2.4"]
                  [slingshot "0.12.2"]
-                 [org.cyverse/clj-jargon "2.8.0"]
+                 [org.cyverse/clj-jargon "2.8.2"]
                  [org.cyverse/clojure-commons "2.8.0"]
                  [org.cyverse/common-cli "2.8.1"]])
